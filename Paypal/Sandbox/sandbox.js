623ef19ad4d0f0bbfed1491011eeb9432fab5b68
sachtony <duongngoclinh.htd@gmail.com>
2022-05-04 09:45:57 +0700

    Update sandbox.js

--- Paypal/Sandbox/sandbox.js
+++ Paypal/Sandbox/sandbox.js
@@ -1,1 +1,8 @@
-
+import { loadScript } from "@paypal/paypal-js";
+loadScript({ "client-id": ATVr_gSJ6V1ZZ79_5sFCZZJ_W-MGrV5UQpc45wT0oMv8iXluL7IzRLsLJHNGSYE4qVqamXNzrnxIGUro })
+.then((paypal) => {
+    // start to use the PayPal JS SDK script
+})
+.catch((err) => {
+    console.error("failed to load the PayPal JS SDK script", err);
+});

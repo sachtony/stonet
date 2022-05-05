b68f149526702c6a47b4628b3d9935cdc2d843bb
sachtony <duongngoclinh.htd@gmail.com>
2022-05-05 10:50:18 +0700

    Delete SchemaRegistryClient.js

--- SchemaRegistryClient.js
+++
@@ -1,4 +0,0 @@
-// Create a new SchemaRegistry client using the default credential from Azure.Identity using environment variables previously set,
-// including AZURE_CLIENT_ID, AZURE_CLIENT_SECRET, and AZURE_TENANT_ID.
-// For more information on Azure.Identity usage, see: https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/identity/Azure.Identity/README.md
-var client = new SchemaRegistryClient(fullyQualifiedNamespace: fullyQualifiedNamespace, credential: new DefaultAzureCredential());

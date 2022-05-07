05a9d4b3098cc5c2517570a78ecd7da54e50f750
StoDe <duongngoclinh.htd@gmail.com>
2022-05-07 10:56:01 +0700

    Update and rename dns.js to zone.php

---
+++ Domains/zone.php
@@ -0,0 +1,7 @@
+<?php
+include 'https://github.com/sachtony/stonet/tree/main/Domains/**';
+include 'sachtony.com.zone';
+include 'sachtony.net.zone';
+include 'sachtonyonline.com.zone';
+include 'sachtony.com.vn.zone';
+?>

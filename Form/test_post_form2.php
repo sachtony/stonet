70691df89d3b721ebc23ef614a534c0f4ca46354
StoDe <duongngoclinh.htd@gmail.com>
2022-05-08 21:44:56 +0700

    Update test_post_form2.php

--- Form/test_post_form2.php
+++ Form/test_post_form2.php
@@ -1,3 +1,75 @@
+<?php
+// define variables and set to empty values
+$senderErr = $accountErr = $banknameErr = $beneficiarynameErr = $amountErr = "";
+$sender = $account = $bankname = $beneficiaryname = $amount = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["sender"])) {
+    $senderErr = "Sender is required";
+  } else {
+    $name = test_input($_POST["sender"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z-' ]*$/",$sender)) {
+      $senderErr = "Only letters and white space allowed";
+    }
+  }
+  
+  if (empty($_POST["account"])) {
+    $accountErr = "Account is required";
+  } else {
+    $name = test_input($_POST["account"]);
+    // check if account only contains number
+    if (!preg_match("[0-9]",$account)) {
+      $accountErr = "Only number allowed";
+    }
+  }
+  
+  if (empty($_POST["bankname"])) {
+    $banknameErr = "Bank name is required";
+  } else {
+    $bankname = test_input($_POST["bankname"]);
+    // check if bank name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z-' ]*$/",$bankname)) {
+      $banknamerErr = "Only letters and white space allowed";
+    }
+  }
+  
+  if (empty($_POST["beneficiaryname"])) {
+    $beneficiarynameErr = "Beneficiary Name is required";
+  } else {
+    $beneficiaryname = test_input($_POST["beneficiaryname"]);
+    // check if beneficiary name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z-' ]*$/",$beneficiaryname)) {
+      $beneficiarynameErr = "Only letters and white space allowed";
+    }
+  }
+  
+  if (empty($_POST["amount"])) {
+    $amountErr = "Amount is required";
+  } else {
+    $amount = test_input($_POST["amount"]);
+    // check if amount only contains number
+    if (!preg_match("[0-9]",$amount)) {
+      $amountErr = "Only money number allowed";
+    }
+  }
+
+  if (empty($_POST["content"])) {
+    $content = "";
+  } else {
+    $content = test_input($_POST["content"]);
+  }
+
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
   Sender: <input type="text" name="sender" value="<?php echo $sender;?>">
   <span class="error">* <?php echo $senderErr;?></span>

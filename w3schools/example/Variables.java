1c1d1e3d57113daea1acd90bf0874320e983dc65
StoDe <duongngoclinh.htd@gmail.com>
2022-05-11 14:53:25 +0700

    Create Variables.java

---
+++ w3schools/example/Variables.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    float x;
+    float i;
+    float z;
+    x = 0.51f;
+    i = 5000f;
+    z = x + i;
+    x = z;
+    System.out.println(x); // Print the value of x + i
+  }
+}

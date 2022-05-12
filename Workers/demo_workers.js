fc7a0daf698fe02a7f240e1b7af297ac29fc9ee1
StoDe <duongngoclinh.htd@gmail.com>
2022-05-12 10:49:15 +0700

    Update demo_workers.js

--- Workers/demo_workers.js
+++ Workers/demo_workers.js
@@ -10,3 +10,5 @@ var accountid = "818356f2dccbaaeaba2b09d356fc9441";
 elem = document.getElementById("worker"); // Find an element 
 elem.innerHTML = accountid;           // Display x in the element
 
+var servive.worker.router = "jobsker.1010101.workers.dev";
+

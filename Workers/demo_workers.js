1e6e894729fba80c477368e0a577e198efc2204b
StoDe <duongngoclinh.htd@gmail.com>
2022-05-12 09:58:44 +0700

    Update demo_workers.js

--- Workers/demo_workers.js
+++ Workers/demo_workers.js
@@ -1,9 +1,12 @@
 var i = 0;
-
 function timedCount() {
   i = i + 1;
   postMessage(i);
   setTimeout("timedCount()",500);
 }
-
 timedCount();
+
+var accountid = "818356f2dccbaaeaba2b09d356fc9441";
+elem = document.getElementById("worker"); // Find an element 
+elem.innerHTML = accountid;           // Display x in the element
+

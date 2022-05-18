2067c8f8a5c9aa9311b064a2fb18f284fcf9e34f
StoDe <duongngoclinh.htd@gmail.com>
2022-05-18 08:26:16 +0700

    Update template.js

--- page/template.js
+++ page/template.js
@@ -1,7 +1,7 @@
-let header = ["#nav"];
+let header = ["#pay"];
 let tags = ["#paypal", "#bank", "#card"];
 
-let html = `<h2>${header}</h2><ul>`;
+let html = `${header}<ul>`;
 
 for (const x of tags) {
   html += `<li>${x}</li>`;

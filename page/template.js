187845a7ecbbd587d363f4101108d4e45d62cb4a
StoDe <duongngoclinh.htd@gmail.com>
2022-05-18 08:38:01 +0700

    Update template.js

--- page/template.js
+++ page/template.js
@@ -1,11 +1,29 @@
-let header = ["#pay"];
-let tags = ["#paypal", "#bank", "#card"];
+function NavHeader() {
+    let header = ["#pay"];
+    let tags = ["#paypal", "#bank", "#card"];
 
-let html = `${header}<ul>`;
+    let html = `${header}<ul>`;
 
-for (const x of tags) {
-  html += `<li>${x}</li>`;
+    for (const x of tags) {
+      html += `<li>${x}</li>`;
+    }
+
+    html += `</ul>`;
+    document.getElementById("#").innerHTML = html;
+};
+
+function myPrice() {
+    let price = 10;
+    let VAT = 0.25;
+    let total = `Total: ${(price * (1 + VAT)).toFixed(2)}`;
+
+    document.getElementById("#").innerHTML = total;
+};
+
+function getBalance() {
+    let x = 0.51;
+    let amount = 5000.00;
+    let getX = `${(x + amount).toFixed(2)}`;
+
+    document.getElementById("#").innerHTML = getX;
 }
-
-html += `</ul>`;
-document.getElementById("#").innerHTML = html;

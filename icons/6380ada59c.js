675d2b16a93a6d4f365a08f12ed354f51a92813c
StoDe <duongngoclinh.htd@gmail.com>
2022-05-21 20:08:11 +0700

    Delete 6380ada59c.js

--- icons/6380ada59c.js
+++
@@ -1,7 +0,0 @@
-set src="https://kit.fontawesome.com/6380ada59c.js";
-function getUpdate(icons) {
-  var scr = "https://npm.fontawesome.com/**";
-  document.getElementById("fortawesome").innerHTML = icons;
-}
-
-myIcons();

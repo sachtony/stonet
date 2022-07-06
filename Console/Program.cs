61b2429ef04105af723325feaf12d73bcb7b6d3c
StoDe <duongngoclinh.htd@gmail.com>
2022-07-06 16:37:38 +0700

    Update Program.cs

--- Console/Program.cs
+++ Console/Program.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp
+namespace SachtonyApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("Network connect with SHA256:fQjpbbuyJBuGw9/HZGT+VNWahlcFdGGsYLzMk/al9kQ");
         }
     }
 }

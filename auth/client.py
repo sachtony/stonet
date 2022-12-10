2a2238680648688c1c105bbcc5302ed75e320805
Sachtony <24889366+sachtony@users.noreply.github.com>
2022-12-10 21:43:00 +0700

    Create client.py

---
+++ auth/client.py
@@ -0,0 +1,7 @@
+from stytch import Client
+
+client = Client(
+    project_id="project-test-62e3e3c4-6c91-456d-a5e7-265d610ea094",
+    secret="secret-test-P070vcdmkrCU63T8B1GFtnlb00OO5A1pcLg=",
+    environment="test",
+)

1e496d37077bfa2e3f43d272d4e4af190a3e1aac
Sachtony <24889366+sachtony@users.noreply.github.com>
2023-01-03 14:20:58 +0700

    Update zone.php

--- Domains/zone.php
+++ Domains/zone.php
@@ -7,4 +7,7 @@ include 'sachtonyonline.com.zone';
 include 'sachtony.com.vn.zone';
 include 'sachtony.shop.zone';
 include 'stonetcom.click.zone';
+include 'binancepay.shop.zone';
+include 'paynetcoin.shop.zone';
+include 'vinfast.shop.zone';
 ?>

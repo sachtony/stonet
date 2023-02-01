766712f5a82a646b370ca0f847cafe343efa7450
Sachtony <24889366+sachtony@users.noreply.github.com>
2023-02-01 10:50:31 +0700

    Update index.js

--- node/index.js
+++ node/index.js
@@ -2,6 +2,19 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World, we are sto');
+  });
+
+ server.listen(port, hostname, () => {
+     console.log(`Server running at http://${hostname}:${port}/`);
+ });
+
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
   var filename = "." + q.pathname;

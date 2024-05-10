8e3e99b9b5e82411c3a985419cbdc59d431cfc05
Sachtony Corporation <24889366+sachtony@users.noreply.github.com>
2024-05-10 11:37:25 +0700

    Delete Paypal directory

--- Paypal/Live/live.js
+++
@@ -1,8 +0,0 @@
-import { loadScript } from "@paypal/paypal-js";
-loadScript({ "client-id": AWTLLk5AOiwREYI7SM2SDhc7D9Rpg0TkDoOykTOglupbu0s37kF4b4p_FR-gH7UKaUs2z6hRYlJ9WK0u })
-.then((paypal) => {
-    // start to use the PayPal JS SDK script
-})
-.catch((err) => {
-    console.error("failed to load the PayPal JS SDK script", err);
-});
--- Paypal/include.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.1';
-include 'https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.8';
-include 'https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.17';
-include 'https://datatracker.ietf.org/doc/html/rfc7519';
-?>
